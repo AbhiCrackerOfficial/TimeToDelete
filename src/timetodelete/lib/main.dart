74bb6ffdf48b2dc5195c73d4a9cbec3da0952a45
ABHI <abhishekkumar.ac001@gmail.com>
2024-05-06 19:35:09 +0530

    Added Basic Sorting (Time & Alpha) & Fixed Some Issues [Removed RiverPod Because Not Using it]

--- src/timetodelete/lib/main.dart
+++ src/timetodelete/lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:timetodelete/pages/layout.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
-
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const ProviderScope(child: Layout()));
+  runApp(const Layout());
 }
